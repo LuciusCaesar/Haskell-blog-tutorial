be1ce8cbefc60f65624b8f80aa254b87d67530f2
Lucius Caesar <lucien.culot@gmail.com>
2024-12-10 23:49:18 +0100

    Representing the markup language as a Haskell data type

---
+++ Markup.hs
@@ -0,0 +1,22 @@
+module Markup
+  ( Document,
+    Structure
+      ( Heading,
+        Paragraph,
+        UnorderedList,
+        OrderedList,
+        CodeBlock
+      ),
+  )
+where
+
+import Numeric.Natural
+
+type Document = [Structure]
+
+data Structure
+  = Heading Natural String
+  | Paragraph String
+  | UnorderedList [String]
+  | OrderedList [String]
+  | CodeBlock [String]
