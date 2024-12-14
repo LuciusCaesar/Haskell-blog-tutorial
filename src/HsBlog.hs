d91a214bf711267699860a116cb7387e1254a3a1
Lucius Caesar <lucien.culot@gmail.com>
2024-12-14 22:11:03 +0100

    Fancy options parsing

--- src/HsBlog.hs
+++ src/HsBlog.hs
@@ -1,73 +1,23 @@
-module HsBlog (
-  main, 
-  process) 
+-- HsBlog.hs
+module HsBlog
+  ( convertSingle,
+    convertDirectory,
+    process,
+  )
 where
 
-import qualified HsBlog.Markup as Markup
-import qualified HsBlog.Html as Html
 import HsBlog.Convert (convert)
-import System.Directory (doesFileExist)
-import System.Directory.Internal.Prelude (getArgs)
+import qualified HsBlog.Html as Html
+import qualified HsBlog.Markup as Markup
+import System.IO
 
-main :: IO ()
--- main =
---   getArgs >>= \case
---     [] ->
---       getContents >>= \content ->
---         putStrLn (process "Empty title" content)
---     [input, output] ->
---       doesFileExist input >>= \exist ->
---         if exist
---           then
---             readFile input >>= \content ->
---               whenIO confirm $ writeFile output $ process input content
---           else putStrLn "file does not exist"
---     _ -> putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
-main = do
-  args <- getArgs
-  case args of
-    [] -> do
-      content <- getContents
-      putStrLn (process "Empty title" content)
-    [input, output] -> do
-      exist <- doesFileExist input
-      if exist
-        then do
-          content <- readFile input
-          whenIO confirm $ writeFile output $ process input content
-        else putStrLn "file does not exist"
-    _ -> putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
+convertSingle :: Html.Title -> Handle -> Handle -> IO ()
+convertSingle title input output = do
+  content <- hGetContents input
+  hPutStrLn output (process title content)
 
+convertDirectory :: FilePath -> FilePath -> IO ()
+convertDirectory = error "Not implemented"
 
 process :: Html.Title -> String -> String
 process title = Html.render . convert title . Markup.parse
-
-whenIO :: IO Bool -> IO () -> IO ()
--- whenIO cond action =
---   cond >>= \result ->
---     if result
---       then action
---       else pure ()
-whenIO cond action = do
-  result <- cond
-  if result then action else pure ()
-
-confirm :: IO Bool
--- confirm =
---   putStrLn "Are you sure? (y/n)"
---     *> getLine
---     >>= \case
---       "y" -> pure True
---       "n" -> pure False
---       _ ->
---         putStrLn "Invalid response. use y or n"
---           *> confirm
-confirm = do
-  putStrLn "Are you sure? (y/n)"
-  line <- getLine
-  case line of
-    "y" -> pure True
-    "n" -> pure False
-    _ -> do
-      putStrLn "Invalid response, use y or no"
-      confirm
