fbdbc4a09d110446c8916affac0e08827ae4321a
Lucius Caesar <Lucien.Culot@gmail.com>
2024-12-28 13:40:24 +0100

    Exceptions

--- app/Main.hs
+++ app/Main.hs
@@ -3,42 +3,69 @@
 -- | Entry point for the hs-blog-gen program
 module Main where
 
-import qualified HsBlog
 import OptParse
-import System.Directory (doesFileExist)
-import System.Exit (exitFailure)
 import System.IO
+import Control.Exception (bracket)
+import HsBlog
 
+-- main :: IO ()
+-- main = do
+--   options <- parse
+--   case options of
+--     ConvertDir input output ->
+--       HsBlog.convertDirectory input output
+--     ConvertSingle input output -> do
+--       (title, inputHandle) <-
+--         case input of
+--           Stdin ->
+--             pure ("", stdin)
+--           InputFile file ->
+--             (,) file <$> openFile file ReadMode
+
+--       outputHandle <-
+--         case output of
+--           Stdout -> pure stdout
+--           OutputFile file -> do
+--             exists <- doesFileExist file
+--             shouldOpenFile <-
+--               if exists
+--                 then confirm
+--                 else pure True
+--             if shouldOpenFile
+--               then openFile file WriteMode
+--               else exitFailure
+
+--       HsBlog.convertSingle title inputHandle outputHandle
+--       hClose inputHandle
+--       hClose outputHandle
 main :: IO ()
 main = do
   options <- parse
   case options of
     ConvertDir input output ->
       HsBlog.convertDirectory input output
-    ConvertSingle input output -> do
-      (title, inputHandle) <-
-        case input of
-          Stdin ->
-            pure ("", stdin)
-          InputFile file ->
-            (,) file <$> openFile file ReadMode
-
-      outputHandle <-
-        case output of
-          Stdout -> pure stdout
-          OutputFile file -> do
-            exists <- doesFileExist file
-            shouldOpenFile <-
-              if exists
-                then confirm
-                else pure True
-            if shouldOpenFile
-              then openFile file WriteMode
-              else exitFailure
-
-      HsBlog.convertSingle title inputHandle outputHandle
-      hClose inputHandle
-      hClose outputHandle
+    ConvertSingle input output ->
+      let
+        withInputHandle :: (String -> Handle -> IO a) -> IO a
+        withInputHandle action =
+          case input of
+            Stdin ->
+              action "" stdin
+            InputFile file ->
+              bracket
+                (openFile file ReadMode)
+                hClose
+                (action file)
+        withOutputHandle :: (Handle -> IO a) -> IO a
+        withOutputHandle action =
+          case output of
+            Stdout -> action stdout
+            OutputFile file ->
+              bracket
+                (openFile file WriteMode)
+                hClose
+                action
+      in withInputHandle (\title -> withOutputHandle . HsBlog.convertSingle title)
 
 ------------------------------------------------
 
